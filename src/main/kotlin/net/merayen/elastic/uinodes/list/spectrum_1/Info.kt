ec292665e0352e8824127262b1c1b4534aabe1c9
Merayen Salberg <pigzelner@gmail.com>
2020-04-18 11:23:05 +0200

    Delete spectrum_1-node as we use frequency_1-node

--- src/main/kotlin/net/merayen/elastic/uinodes/list/spectrum_1/Info.kt
+++
@@ -1,9 +0,0 @@
-package net.merayen.elastic.uinodes.list.spectrum_1
-
-import net.merayen.elastic.uinodes.BaseInfo
-
-class Info : BaseInfo {
-	override fun getName() = "Spectrum"
-	override fun getDescription() = "Frequency spectrum"
-	override fun getCategories() = arrayOf("measurement")
-}
